d296b2953bc0efc1a8939c7e40ad4abda47da254
Fayyaz Alam <fayazalam.98@gmail.com>
2021-06-30 19:48:16 -0700

    Q4 try not complete

--- app.js
+++ app.js
@@ -65,4 +65,15 @@
 // ];
 
 
+//Question 4
+
+// function Population(name, gender, address, education, profession,){
+//     this.name=name, 
+//     this.gender=gender,
+//     this.address=address,
+//     this.education=education,
+//     this.profession=profession
+// }
+
+// not cpmpleted
 
